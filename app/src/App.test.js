311c9a6561b5c380dd90a407eb3c8b470e72e70b
iskilled <iskilled@gmail.com>
2018-04-13 19:22:23 +0100

    Add tests for login form
    https://egghead.io/lessons/react-replicate-user-activity-with-faker-and-puppeteer

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -2,15 +2,23 @@
  * @jest-environment node
  */
 const puppeteer = require('puppeteer');
+const faker = require('faker');
+
+const user = {
+  email: faker.internet.email(),
+  password: 'test',
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+};
 
 const isDebugging = () => {
   const debugging_mode = {
       headless: false,
-      slowMo: 250,
+      slowMo: 50,
       devtools: true,
   };
   return process.env.NODE_ENV === 'debug' ? debugging_mode : {};
-}
+};
 
 let browser;
 let page;
@@ -37,8 +45,25 @@ describe('on page load', () => {
     
     expect(navbar).toBeTruthy();
     expect(listItems.length).toBe(4);
-  })
+  });
 
+  test('login form works correctly', async () => {
+    await page.click('[data-testid="firstName"]');
+    await page.type('[data-testid="firstName"]', user.firstName);
+
+    await page.click('[data-testid="lastName"]');
+    await page.type('[data-testid="lastName"]', user.lastName);
+
+    await page.click('[data-testid="email"]');
+    await page.type('[data-testid="email"]', user.email);
+
+    await page.click('[data-testid="password"]');
+    await page.type('[data-testid="password"]', user.password);
+
+    await page.click('[data-testid="submit"]');
+
+    await page.waitForSelector('[data-testid="success"]');
+  }, 16000);
 });
 
 afterAll(() => {
---
+++ app/src/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+ import './Login.css';
+
+export default function Login(props) {
+  return (
+    <div className="login-page">
+      <div className="form">
+        <form onSubmit={props.submit} className="formSignin">
+          <input data-testid="firstName" type="text" placeholder="First Name" className="form-control" />
+          <input data-testid="lastName" type="text" placeholder="Last Name" className="form-control" />
+          <input data-testid="email" type="text" placeholder="Email" className="form-control" />
+          <input data-testid="password" type="password" placeholder="password" className="form-control" />
+          <div>
+            <button data-testid="submit" className="form-control">login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
